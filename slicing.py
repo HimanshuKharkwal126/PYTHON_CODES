d5c7cfa0725e60f6f73b86e91519f23f5e9561bf
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-21 21:21:49 +0530

    adding program

---
+++ slicing.py
@@ -0,0 +1,8 @@
+list=["harsh","rahul","himanshu","bhuppi","yogesh","saurav"]
+print(list[5:2:-1])
+print()
+print(list[-2:-5:-1])
+print()
+
+for i in range(len(list)):
+    print(list[i],"is at",i,"index")

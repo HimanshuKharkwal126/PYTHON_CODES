efbab290d20a82ba04d50fe2c8721599a996d95b
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-22 21:05:20 +0530

    adding program

---
+++ time_period.py
@@ -0,0 +1,11 @@
+
+# difference between two time period
+
+
+t1=int(input("enter the no:"))
+t2=int(input("enter the no:"))
+h1=t1//100
+m1=t1%100
+h2=t2//100
+m2=t2%100
+print("difference is ",h2 - h1,":",m2 - m1)

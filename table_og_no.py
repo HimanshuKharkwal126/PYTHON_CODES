1868bace61fe4e606add117fa6fa466aca941ac1
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-24 20:42:01 +0530

    adding program

---
+++ table_og_no.py
@@ -0,0 +1,30 @@
+# n=int(input("enter the number:"))
+# for i in range(1,11,1):
+#     print(n,"*",i,"=",n*i)
+
+
+# print()
+# print()
+# print()
+
+
+
+# table of miles into kilometers:
+
+
+
+# print("km   miles")
+# for i in range(0,101,10):
+#     print(i," ",round(i-1.6,2))
+    
+    
+# for num in range(1, 11):
+#     if num == 6:
+#         continue
+#     print(num)
+
+for num in range(1, 6):
+    if num == 3:
+        pass
+    else:
+        print(num)

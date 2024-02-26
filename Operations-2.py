7c13290103e25fd8f1919bb4563b3ff351055df2
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-26 22:16:18 +0530

    adding program

---
+++ Operations-2.py
@@ -0,0 +1,39 @@
+
+a="himanshu"
+print(a[3:7+1])                   # print letters in given range
+print()
+print(a[:3])                      # print first 3 letters of string
+print()
+print(a[3:])                      # print last letters of string except first 3 letters
+print()
+print(a[::-1])                    # reversing the string
+
+
+b="himanshu is a student"
+print("upper case format is:",b.upper())
+print()
+print("lower case format is:",b.lower())
+print()
+print("swap case format is:",b.swapcase())
+print()
+print("capitalize format is:",b.capitalize())
+print()
+print("title case format is:",b.title())
+print()
+
+c="rahul"
+d="abc147"
+print(c.isupper())                # for upper case
+print()
+print(c.islower())                # for lower case
+print()
+print(d.isalnum())                # for alphabet and number
+print()
+
+e="abc123"
+print(e.isalpha())                 # for only alphabet
+print()
+
+f="harshwardhan"
+print(f.find("r"))                 # for finding the particular letter
+print()

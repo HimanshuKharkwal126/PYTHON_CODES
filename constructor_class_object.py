01caa7b5d0d65422a84052bc649b5f2e9e478df9
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-27 21:21:29 +0530

    adding program

---
+++ constructor_class_object.py
@@ -0,0 +1,36 @@
+
+# Constructor:
+
+"""
+
+class Student:
+    def __init__(self,a,b,c):
+        self.name=a
+        self.standard=b
+        self.school=c
+y=Student("Yuvraj","B.Com 1st year","Asian Academy")
+h=Student("Himanshu","B.Tech 1st year","Daya Sagar Inter College")
+print(y.name,"is studying in",y.standard,"and he was in",y.school)
+print(h.name,"is studying in",h.standard,"and he was in",h.school)
+
+"""
+
+# Class and Object:
+
+
+
+class Student:
+    name="Himanshu"
+    standard="B.Tech"
+    school="SIT"
+y=Student()
+y.name="Yuvraj"
+y.standard="12th"
+y.school="Asian Academy"
+h=Student()
+h.name="Himanshu"
+h.standard="12th"
+h.school="Daya Sagar"
+print("In 2022",y.name,"was studing in",y.standard,"in",y.school,"school")
+print("In 2022",h.name,"was studing in",h.standard,"in",h.school,"school")
+

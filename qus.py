f7075247854af095e3dbe851abb018647ba11cda
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-27 21:23:23 +0530

    adding program

---
+++ qus.py
@@ -0,0 +1,49 @@
+
+# Calculate volume of cuboid:
+"""
+def cuboid(l,b,h):
+    return l*b*h
+print("volume of cuboid is:",cuboid(2,5,3))
+
+"""
+
+# find cube of given no:
+"""
+def cube(a=3):
+    return a*a*a
+print("cube of given no with function(cube of 3):",cube())
+print("cube of given no without function(cube of 4):",cube(4))
+
+"""
+
+# comparision of two character arguments:
+"""
+def cmp(a,b):
+    if a==b:
+        return True
+    else:
+        return False
+print(cmp("a","a"))
+
+"""
+
+# compare the length of two strings:
+"""
+def cmp(a,b):
+    if len(a)==len(b):
+        return True
+    else:
+        return False
+a=input("enter the first string:")
+b=input("enter the second string:")
+print(cmp(a,b))
+
+"""
+
+# write a function who returns nth root x^(1/n):
+"""
+def root(x,n=2):
+    return x**(1/n)                              #  ( ** ) used for power of x.
+print(root(5))
+
+"""

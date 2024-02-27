d802232e3d2a0ae753867b67663a15ed1fea369d
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-27 21:20:01 +0530

    adding program

---
+++ Bitwise_Operator.py
@@ -0,0 +1,7 @@
+print(25&17)            # & { bitwise And operator }
+print(25|17)            # | { bitwise Or operator }
+print(25^17)            # ^ { bitwise Xor operator }
+print(~7)               # ~ { bitwise Not operator (positive number) it will saved as 2s compliment }
+print(~-15)             # ~- { bitwise Not operator (negative number) it will saved as 2s compliment }
+print(8<<2)             # << { bitwise right shift operator }
+print(8>>2)             # >> { bitwise left shift operator }

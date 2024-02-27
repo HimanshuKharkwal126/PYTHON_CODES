a420f29152d78a189935f02f41185381e0870272
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-02-27 21:22:26 +0530

    adding program

---
+++ Operations-1.py
@@ -0,0 +1,37 @@
+
+# concatination
+
+
+
+str1="123"
+str2="258"                           # add both string as( 123258 )
+print(str1+str2)
+print()
+
+
+str="123"
+print(str*3)                      # print str 3 times( 123123123 )
+print()
+
+
+# on the basis of ASCII values:
+s1="rahul"
+s2="pranjal"
+print(s1>s2)                      # give the result as True or False on the basis of Ascii value:
+print()
+
+
+print(ord('A'))                   # print Ascii value of A ( 65 )
+print(chr(97))                    # print assigned value to 97  ( a )
+
+
+a=97
+print(bin(a))                     # print binary value
+print(oct(a))                     # print octal value
+print(hex(a))                     # print hexadecimal value
+
+b=0o57                            # print octal value of 57
+print(b)
+print(hex(b))                     # change octal value to hexadecimal value
+print()
+
